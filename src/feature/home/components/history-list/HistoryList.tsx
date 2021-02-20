19b1cd1455a0cf2378e6d9244b35914b95dfb258
HyunnoH <hyunnoh01@gmail.com>
2021-02-20 17:36:31 +0900

    Add history list component

---
+++ src/feature/home/components/history-list/HistoryList.tsx
@@ -0,0 +1,16 @@
+/** @jsx jsx */
+import {jsx, css} from '@emotion/core'
+
+interface HistoryListProps {
+  
+}
+
+export default function HistoryList({}: HistoryListProps) {
+  return <div>
+    
+  </div>
+}
+
+const styles = {
+  wrapper: css``
+}
